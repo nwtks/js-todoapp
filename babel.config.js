13e774bbe74f99a63918b78ebe3fb2bb8ffb2536
nwtks <44502906+nwtks@users.noreply.github.com>
2018-10-28 13:05:58 +0900

    babel

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-  presets: ['@babel/preset-env'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: false,
+        targets: {
+          ie: '11'
+        }
+      }
+    ]
+  ],
   plugins: [
     [
       'transform-react-jsx',
